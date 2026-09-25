4c8ab71d2151a127c4ac32267b8e808cfab567a0
SlimmySlabby <seancantwell@protonmail.com>
2026-09-25 02:24:49 -0500

    Restructure code

--- src/main/java/com/slimmyslabby/cb/CoalBricksMod.java
+++ src/main/java/com/slimmyslabby/cb/CoalBricksMod.java
@@ -3,18 +3,13 @@ package com.slimmyslabby.cb;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.item.ItemStack;
-import net.minecraft.init.Items;
 
 @Mod(modid = CoalBricksMod.MODID, name = CoalBricksMod.NAME, version = CoalBricksMod.VERSION)
 public class CoalBricksMod {
 
     public static final String MODID = "cb";
     public static final String NAME = "Coal Bricks";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
@@ -23,9 +18,6 @@ public class CoalBricksMod {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        final ResourceLocation registryName = new ResourceLocation(MODID, "coal_brick_recipe");
-        final ResourceLocation recipeGroup = new ResourceLocation(MODID, "recipes");
-        final ItemStack output = new ItemStack(ModItems.COAL_BRICK);
-        GameRegistry.addShapedRecipe(registryName, recipeGroup, output, "CC", "CC", 'C', Items.COAL);
+        ModRecipes.addCraftingRecipes();
     }
 }
---
+++ src/main/java/com/slimmyslabby/cb/ModRecipes.java
@@ -0,0 +1,16 @@
+package com.slimmyslabby.cb;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModRecipes
+{
+    public static void addCraftingRecipes() {
+        final ResourceLocation registryName = new ResourceLocation(CoalBricksMod.MODID, "coal_brick_recipe");
+        final ResourceLocation recipeGroup = new ResourceLocation(CoalBricksMod.MODID, "recipes");
+        final ItemStack output = new ItemStack(ModItems.COAL_BRICK);
+        GameRegistry.addShapedRecipe(registryName, recipeGroup, output, "CC", "CC", 'C', Items.COAL);
+    }
+}
